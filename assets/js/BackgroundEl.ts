02860d21a6d66bf5d70e8c658d094521715604f4
迷渡 <justjavac@gmail.com>
2021-06-22 09:33:36 +0800

    添加 Cloud 和 Background
    Signed-off-by: 迷渡 <justjavac@gmail.com>

---
+++ assets/js/BackgroundEl.ts
@@ -0,0 +1,146 @@
+import { IS_HIDPI } from "./constants";
+import { getRandomNum } from "./utils";
+
+/**
+ * Background element object config.
+ * Real values assigned when game type changes.
+ * @enum {number}
+ */
+enum BackgroundElConfig {
+  MAX_BG_ELS = 0,
+  MAX_GAP = 0,
+  MIN_GAP = 0,
+  POS = 0,
+  SPEED = 0,
+  Y_POS = 0,
+  MS_PER_FRAME = 0, // only needed when BACKGROUND_EL.FIXED is true
+}
+
+export default class BackgroundEl {
+  canvas: HTMLCanvasElement;
+  canvasCtx: CanvasRenderingContext2D;
+  spritePos: object;
+  containerWidth: number;
+  xPos: number;
+  yPos: number;
+  remove: boolean;
+  type: string;
+  gap: number;
+  animTimer: number;
+  switchFrames: boolean;
+  spriteConfig: object;
+
+  /**
+   * 背景元素
+   * 和云类似(cloud)，但是没有随机的 y 坐标
+   * @param {HTMLCanvasElement} canvas Canvas element.
+   * @param {Object} spritePos Position of image in sprite.
+   * @param {number} containerWidth
+   * @param {string} type Element type.
+   */
+  constructor(
+    canvas: HTMLCanvasElement,
+    spritePos: object,
+    containerWidth: number,
+    type: string,
+  ) {
+    this.canvas = canvas;
+    this.canvasCtx = /** @type {CanvasRenderingContext2D} */ this.canvas
+      .getContext("2d");
+    this.spritePos = spritePos;
+    this.containerWidth = containerWidth;
+    this.xPos = containerWidth;
+    this.yPos = 0;
+    this.remove = false;
+    this.type = type;
+    this.gap = getRandomNum(
+      BackgroundElConfig.MIN_GAP,
+      BackgroundElConfig.MAX_GAP,
+    );
+    this.animTimer = 0;
+    this.switchFrames = false;
+
+    this.spriteConfig = {};
+    this.init();
+  }
+
+  /**
+   * Initialise the element setting the y position.
+   */
+  init() {
+    this.spriteConfig = Runner.spriteDefinition.BACKGROUND_EL[this.type];
+    if (this.spriteConfig.FIXED) {
+      this.xPos = this.spriteConfig.FIXED_X_POS;
+    }
+    this.yPos = BackgroundElConfig.Y_POS -
+      this.spriteConfig.HEIGHT +
+      this.spriteConfig.OFFSET;
+    this.draw();
+  }
+
+  draw() {
+    this.canvasCtx.save();
+    let sourceWidth = this.spriteConfig.WIDTH;
+    let sourceHeight = this.spriteConfig.HEIGHT;
+    let sourceX = this.spriteConfig.X_POS;
+    const outputWidth = sourceWidth;
+    const outputHeight = sourceHeight;
+
+    if (IS_HIDPI) {
+      sourceWidth *= 2;
+      sourceHeight *= 2;
+      sourceX *= 2;
+    }
+
+    this.canvasCtx.drawImage(
+      Runner.imageSprite,
+      sourceX,
+      this.spritePos.y,
+      sourceWidth,
+      sourceHeight,
+      this.xPos,
+      this.yPos,
+      outputWidth,
+      outputHeight,
+    );
+
+    this.canvasCtx.restore();
+  }
+
+  /**
+   * 更新背景元素位置
+   * @param speed 速度
+   */
+  update(speed: number) {
+    // 如果该元素已经移除了，不处理
+    if (this.remove) {
+      return;
+    }
+
+    if (!this.spriteConfig.FIXED) {
+      // Fixed speed, regardless of actual game speed.
+      this.xPos -= BackgroundElConfig.SPEED;
+    } else {
+      this.animTimer += speed;
+      if (this.animTimer > BackgroundElConfig.MS_PER_FRAME) {
+        this.animTimer = 0;
+        this.switchFrames = !this.switchFrames;
+      }
+
+      this.yPos = this.switchFrames
+        ? this.spriteConfig.FIXED_Y_POS_1
+        : this.spriteConfig.FIXED_Y_POS_2;
+    }
+    this.draw();
+
+    // 如果在画布上不可见，将其移除
+    if (!this.isVisible()) {
+      this.remove = true;
+    }
+  }
+
+  /** 检查元素是否在舞台(stage)上可见。*/
+  isVisible() {
+    return this.xPos + this.spriteConfig.WIDTH > 0;
+  }
+}
---
+++ assets/js/Cloud.ts
@@ -0,0 +1,113 @@
+import { IS_HIDPI } from "./constants";
+import { getRandomNum } from "./utils";
+
+enum CloudConfig {
+  HEIGHT = 14,
+  MAX_CLOUD_GAP = 400,
+  MAX_SKY_LEVEL = 30,
+  MIN_CLOUD_GAP = 100,
+  MIN_SKY_LEVEL = 71,
+  WIDTH = 46,
+}
+
+export default class Cloud {
+  canvas: HTMLCanvasElement;
+  canvasCtx: CanvasRenderingContext2D;
+  spritePos: object;
+  containerWidth: number;
+  xPos: number;
+  yPos: number;
+  remove: boolean;
+  gap: number;
+
+  /**
+   * 云 ☁️
+   *
+   * 和障碍物(Obstacle)类似，但是没有碰撞盒子。
+   * @param canvas Canvas element.
+   * @param spritePos Position of image in sprite.
+   * @param containerWidth
+   */
+  constructor(
+    canvas: HTMLCanvasElement,
+    spritePos: object,
+    containerWidth: number,
+  ) {
+    this.canvas = canvas;
+    this.canvasCtx = this.canvas.getContext("2d");
+    this.spritePos = spritePos;
+    this.containerWidth = containerWidth;
+    this.xPos = containerWidth;
+    this.yPos = 0;
+    this.remove = false;
+    this.gap = getRandomNum(
+      CloudConfig.MIN_CLOUD_GAP,
+      CloudConfig.MAX_CLOUD_GAP,
+    );
+
+    this.init();
+  }
+
+  /**
+   * Initialise the cloud. Sets the Cloud height.
+   */
+  init() {
+    this.yPos = getRandomNum(
+      CloudConfig.MAX_SKY_LEVEL,
+      CloudConfig.MIN_SKY_LEVEL,
+    );
+    this.draw();
+  }
+
+  /**
+   * Draw the cloud.
+   */
+  draw() {
+    this.canvasCtx.save();
+    let sourceWidth = CloudConfig.WIDTH;
+    let sourceHeight = CloudConfig.HEIGHT;
+    const outputWidth = sourceWidth;
+    const outputHeight = sourceHeight;
+    if (IS_HIDPI) {
+      sourceWidth = sourceWidth * 2;
+      sourceHeight = sourceHeight * 2;
+    }
+
+    this.canvasCtx.drawImage(
+      Runner.imageSprite,
+      this.spritePos.x,
+      this.spritePos.y,
+      sourceWidth,
+      sourceHeight,
+      this.xPos,
+      this.yPos,
+      outputWidth,
+      outputHeight,
+    );
+
+    this.canvasCtx.restore();
+  }
+
+  /**
+   * 更新云的位置 ☁️
+   * @param speed 速度
+   */
+  update(speed: number) {
+    if (this.remove) {
+      return;
+    }
+
+    this.xPos -= Math.ceil(speed);
+    this.draw();
+
+    // 如果在画布上不可见，将其移除
+    if (!this.isVisible()) {
+      this.remove = true;
+    }
+  }
+
+  /** 检查元素是否在舞台(stage)上可见。*/
+  isVisible() {
+    return this.xPos + CloudConfig.WIDTH > 0;
+  }
+}
