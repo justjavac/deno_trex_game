50249fa2bef8d2472c754903812537e52d9e5c06
迷渡 <justjavac@gmail.com>
2022-07-13 13:39:25 +0800

    add license (#3)

--- src/Trex.ts
+++ src/Trex.ts
@@ -1,3 +1,8 @@
+// Copyright (c) 2015 The Chromium Authors. All rights reserved. BSD-style license.
+// https://source.chromium.org/chromium/chromium/src/+/main:LICENSE;drc=0125cf675617075bb4216dc1a794b9038be4f63d
+//
+// Copyright (c) justjavac. All rights reserved. MIT License.
+
 import CollisionBox from "./CollisionBox.ts";
 import { FPS, IS_HIDPI } from "./constants.ts";
 import Runner from "./Runner.ts";
--- src/sprite/Sprite.ts
+++ src/sprite/Sprite.ts
@@ -1,3 +1,8 @@
+// Copyright (c) 2015 The Chromium Authors. All rights reserved. BSD-style license.
+// https://source.chromium.org/chromium/chromium/src/+/main:LICENSE;drc=0125cf675617075bb4216dc1a794b9038be4f63d
+//
+// Copyright (c) justjavac. All rights reserved. MIT License.
+
 import { DPI_TYPE, PIXEL_RATIO } from "../constants.ts";
 import Runner from "../Runner.ts";
 import Config, { Position, Size, SpritePosition } from "./Config.ts";
